3a6d4025b9f89e64cad6219f4ca0c3785c9fcbfe
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-02-01 17:24:53 +0100

    rusk: serialized methods protection tests

--- rusk/tests/services/protected.rs
+++ rusk/tests/services/protected.rs
@@ -90,7 +90,7 @@ fn verify_protected_method(
     }
 }
 
-pub async fn test_protected_method(
+pub fn test_protected_method(
     method: &str,
     contract: ContractId,
     should_discard: bool,
@@ -116,72 +116,92 @@ pub async fn test_protected_method(
     Ok(())
 }
 
-macro_rules! protected_methods_tests {
-    ($($name:ident: $value:expr,)*) => {
-    $(
-        #[tokio::test(flavor = "multi_thread")]
-        pub async fn $name() -> Result<()> {
-            let (method, contract, should_discard) = $value;
-            test_protected_method(method, contract, should_discard).await
-        }
-    )*
-    }
-}
+const TRANSFER_DATA_PROTECTED_METHODS: &'static [&'static str] = &[
+    "root",
+    "num_notes",
+    "module_balance",
+    "message",
+    "opening",
+    "existing_nullifiers",
+    "any_nullifier_exists",
+    "extend_nullifiers",
+    "take_message_from_address_key",
+    "root_exists",
+    "push_message",
+    "take_crossover",
+    "set_crossover",
+    "get_crossover",
+    "extend_notes",
+    "sub_balance",
+    "leaves_from_height",
+    "leaves_from_pos",
+    "push_note",
+    "get_note",
+    "update_root",
+    "add_module_balance",
+    "get_module_balance",
+];
+
+const TRANSFER_LOGIC_PROTECTED_METHODS: &'static [&'static str] = &[
+    "mint",
+    "stct",
+    "wfct",
+    "wfct_raw",
+    "stco",
+    "wfco",
+    "wfco_raw",
+    "wfctc",
+    "root",
+    "num_notes",
+    "module_balance",
+    "message",
+    "opening",
+    "existing_nullifiers",
+    "leaves_from_height",
+    "leaves_from_pos",
+    "spend_and_execute",
+    "refund",
+    "push_note",
+    "update_root",
+    "add_module_balance",
+    "sub_module_balance",
+];
+
+const TRANSFER_PROXY_PROTECTED_METHODS: &'static [&'static str] = &[
+    "spend_and_execute",
+    "refund",
+    "push_note",
+    "update_root",
+    "add_module_balance",
+];
 
 const SHOULD_DISCARD: bool = true;
 const SHOULD_NOT_DISCARD: bool = false; // meaning: should err
 
-protected_methods_tests! {
-    test_protected_method_00: ("root", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_01: ("num_notes", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_02: ("module_balance", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_03: ("message", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_04: ("opening", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_05: ("existing_nullifiers", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_06: ("any_nullifier_exists", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_07: ("extend_nullifiers", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_08: ("take_message_from_address_key", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_09: ("root_exists", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_10: ("push_message", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_11: ("take_crossover", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_12: ("set_crossover", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_13: ("get_crossover", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_14: ("extend_notes", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_15: ("sub_balance", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_16: ("leaves_from_height", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_17: ("leaves_from_pos", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_18: ("push_note", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_19: ("get_note", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_20: ("update_root", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_21: ("add_module_balance", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_22: ("get_module_balance", TRANSFER_DATA_CONTRACT, SHOULD_DISCARD),
+#[tokio::test(flavor = "multi_thread")]
+pub async fn protected_transfer_data_methods() -> Result<()> {
+    for method in TRANSFER_DATA_PROTECTED_METHODS {
+        test_protected_method(*method, TRANSFER_DATA_CONTRACT, SHOULD_DISCARD)?;
+    }
+    Ok(())
+}
 
-    test_protected_method_30: ("mint", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_31: ("stct", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_32: ("wfct", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_33: ("wfct_raw", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_34: ("stco", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_35: ("wfco", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_36: ("wfco_raw", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_37: ("wfctc", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_38: ("root", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_39: ("num_notes", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_40: ("module_balance", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_41: ("message", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_42: ("opening", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_43: ("existing_nullifiers", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_44: ("leaves_from_height", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_45: ("leaves_from_pos", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_46: ("spend_and_execute", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_47: ("refund", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_48: ("push_note", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_49: ("update_root", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_50: ("add_module_balance", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
-    test_protected_method_51: ("sub_module_balance", TRANSFER_LOGIC_CONTRACT, SHOULD_DISCARD),
+#[tokio::test(flavor = "multi_thread")]
+pub async fn protected_transfer_logic_methods() -> Result<()> {
+    for method in TRANSFER_LOGIC_PROTECTED_METHODS {
+        test_protected_method(
+            *method,
+            TRANSFER_LOGIC_CONTRACT,
+            SHOULD_DISCARD,
+        )?;
+    }
+    Ok(())
+}
 
-    test_protected_method_60: ("spend_and_execute", TRANSFER_CONTRACT, SHOULD_NOT_DISCARD),
-    test_protected_method_61: ("refund", TRANSFER_CONTRACT, SHOULD_NOT_DISCARD),
-    test_protected_method_62: ("push_note", TRANSFER_CONTRACT, SHOULD_NOT_DISCARD),
-    test_protected_method_63: ("update_root", TRANSFER_CONTRACT, SHOULD_NOT_DISCARD),
-    test_protected_method_64: ("add_module_balance", TRANSFER_CONTRACT, SHOULD_NOT_DISCARD),
+#[tokio::test(flavor = "multi_thread")]
+pub async fn protected_transfer_proxy_methods() -> Result<()> {
+    for method in TRANSFER_PROXY_PROTECTED_METHODS {
+        test_protected_method(*method, TRANSFER_CONTRACT, SHOULD_NOT_DISCARD)?;
+    }
+    Ok(())
 }
