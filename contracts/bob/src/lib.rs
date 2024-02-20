fe5b501b5bf4efae5afbdfeef5c1369f10e79d78
Eduardo Leegwater Simões <eduardo@dusk.network>
2024-02-20 17:23:30 +0100

    bob: remove `PaymentInfo`

--- contracts/bob/src/lib.rs
+++ contracts/bob/src/lib.rs
@@ -17,20 +17,11 @@ use state::Bob;
 mod wasm {
     use super::*;
 
-    use rusk_abi::PaymentInfo;
-
     #[no_mangle]
     static mut STATE: Bob = Bob::new();
 
     #[no_mangle]
     unsafe fn ping(arg_len: u32) -> u32 {
         rusk_abi::wrap_call(arg_len, |()| STATE.ping())
-    }
-
-    const PAYMENT_INFO: PaymentInfo = PaymentInfo::Transparent(None);
-
-    #[no_mangle]
-    fn payment_info(arg_len: u32) -> u32 {
-        rusk_abi::wrap_call(arg_len, |_: ()| PAYMENT_INFO)
     }
 }
